ccc1beb9283d67a4f931d5831f489be054b67a50
Hajar Aswad <hajaraswadkom@gmail.com>
2022-06-18 10:58:37 +0700

    card

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
+import Card from "./components/Card/Card";
 import NoteForm from "./components/Form/NoteForm";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App bg-slate-900 h-screen w-full text-white">
+    <div className="App bg-slate-900 min-h-screen w-full text-white pb-20">
 
       <header>
         <Navbar />
       </header>
 
-      <main className="flex justify-center mt-10">
-        <NoteForm/>
+      <main className="flex flex-col items-center mt-10 gap-4">
+        <NoteForm />
+
+        <div className="text-left w-10/12 py-4 flex flex-col items-center">
+          <div className="text-left flex w-full">
+            <h1 className="font-semibold text-xl mb-6">Catatan Aktif</h1>
+          </div>
+          <div className="flex flex-wrap justify-between gap-4">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </div>
+
       </main>
 
     </div>
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div className="card p-2 bg-slate-700 sm:w-[15%] md:w-[24%] rounded-md flex flex-col gap-2 shadow-sm">
+            <h4 className="font-semibold text-lg">Judul</h4>
+            <p className="text-slate-500">Waktu</p>
+            <p className="text-thin text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis exercitationem molestiae quia maiores unde placeat, at sint. Voluptates quam pariatur dicta dolorum veritatis, molestias excepturi quae, voluptate, mollitia odio soluta!</p>
+            <div className="flex justify-between mt-4">
+                <button className="text-red-700 hover:bg-red-500 py-1 hover:text-white rounded-md text-center w-full">Delete</button>
+                <button className="text-yellow-700 hover:bg-yellow-500 hover:text-white rounded-md w-full">Arsipkan</button>
+            </div>
+        </div>
+    )
+}
+
+export default Card
