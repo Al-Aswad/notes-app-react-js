72a3e33fcbb971ac82cf83be5daeb5139ab5dd3d
Hajar Aswad <hajaraswadkom@gmail.com>
2022-06-18 11:16:52 +0700

    note

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Card from "./components/Card/Card";
 import NoteForm from "./components/Form/NoteForm";
 import Navbar from "./components/Navbar/Navbar";
+import { getData } from "./utils/data"
 
 function App() {
+  const data = getData();
+
   return (
     <div className="App bg-slate-900 min-h-screen w-full text-white pb-20">
 
@@ -18,14 +21,9 @@ function App() {
             <h1 className="font-semibold text-xl mb-6">Catatan Aktif</h1>
           </div>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {data.map(note => (
+              <Card key={note.id} note={note} />
+            ))}
           </div>
         </div>
 
